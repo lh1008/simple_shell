d754b81e7262613fb7df10359aca0114dc6438db
JamesPagani <jaimeagalvezv@gmail.com>
2019-11-19 22:01:53 +0000

    Headers and Functions for the simple shell.

---
+++ headers.h
@@ -0,0 +1,13 @@
+#ifndef SIMPLE_SHELL
+#define SIMPLE_SHELL
+
+/* Headers used by the shell program */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+/* Functions used by the shell program */
+
+
+#endif
